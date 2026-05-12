bdb539be334e62e35cf85f4cd61056f677f8991f
alinazinchuk1 <dapdpzuuzpzr@gmail.com>
2026-05-12 11:12:41 +0300

    test: додаємо unit-тести для калькулятора

---
+++ src/test_calculator.py
@@ -0,0 +1,6 @@
+﻿import unittest
+from calculator import add, divide
+class TestCalculator(unittest.TestCase):
+    def test_add(self): self.assertEqual(add(2, 3), 5)
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError): divide(10, 0)
